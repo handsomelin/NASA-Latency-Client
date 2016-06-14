5d6400a0167ec624ab05bbdf000540c661ff5426
chiaohao <ksjoey0096@gmail.com>
2016-06-14 03:33:41 -0700

    change to net-ping

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 var https = require('https')
+var ping = require('net-ping')
+
 var exec = require('child_process').exec
 
-var d = {
+var data = {
 	latency: null
 }
 
-var body
-
+/*
 function getData(jsonData, b, callback){
 	exec('ping -c 10 8.8.8.8 | tail -n 1 | sed \'s/\\\// /g\' | awk \'{print $8}\'').stdout.on('data', function(dd){
 		dd = dd.replace(/\n/g,"")
@@ -16,21 +17,38 @@ function getData(jsonData, b, callback){
 		callback(b)
 	})
 }
-
+*/
 var req = new https.request({
 	hostname: "www.google.com",
 	port: 443,
 	path: "/",
-	method: "POST",
-	headers: {
-		"Content-Type": "application, json",
-		"Content-Length": Buffer.byteLength(body)
-	}
+	method: "POST"
 })
-
+/*
 getData(d, body, function(body){
 	req.end(body, function(){
 		console.log(body)
 	})
 })
+*/
 
+var session = ping.createSession()
+
+function getLatency(target, data, callback){
+	session.pingHost(target, function(error, target, sent, rcvd){
+		var ms = rcvd - sent
+		if(error)
+			console.log(target + ": " + error.toString())
+		else{
+			data.latency = ms
+//			console.log(ms)
+		}
+		callback(data)
+	})
+}
+
+getLatency('8.8.8.8', data, function(data){
+	req.end(JSON.stringify(data), function(){
+		console.log(data)
+	})
+})
